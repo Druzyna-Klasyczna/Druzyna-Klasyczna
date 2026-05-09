7d165f5430894140aa07a15cd8aae8d1a7d4154b
Marcel <marcel.pokrywka@gmail.com>
2026-05-09 15:14:56 +0200

    fix tailwind + add to lobby to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import { Lobby } from "./pages/Lobby";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/lobby" element={<Lobby></Lobby>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
