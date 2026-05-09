e35b72fdcf58e702be9fa4e5aa5e87e22a79ecc3
Kubikolo <jakub.kolotylo@gmail.com>
2026-05-09 11:57:40 +0200

    added card type enum

--- backend/game/card.py
+++ backend/game/card.py
@@ -1,6 +1,18 @@
+from enum import Enum
+
+class CardType(Enum):
+    QUESTION = 1
+    POWER_UP = 2
+    DEBUFF = 3
+    EFFECT = 4
+
 class Card:
     def __init__(self):
         pass
+
+    @abstractmethod
+    def get_card_type(self):
+        raise NotImplementedError("this card class shouldn't be used. use a more precise card type.")
 
 class PowerUpCard(Card):
     def __init__(self):
