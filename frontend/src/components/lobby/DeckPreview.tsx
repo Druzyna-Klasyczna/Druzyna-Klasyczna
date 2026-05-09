6897c0dbfb5eefcae6c66d2a718a17c36036e160
Marcel <marcel.pokrywka@gmail.com>
2026-05-09 12:49:20 +0200

    Little broken room page

---
+++ frontend/src/components/lobby/DeckPreview.tsx
@@ -0,0 +1,30 @@
+import { ChevronLeft, ChevronRight, Import } from "lucide-react";
+import { TactileButton } from "../TactileButton";
+
+export const DeckPreview = () => {
+    return (
+        <div className="flex flex-col items-center justify-around h-full py-4 w-full">
+            <div className="flex items-center gap-6">
+                <TactileButton variant="small" color="blue" className="px-2">
+                    <ChevronLeft size={24} />
+                </TactileButton>
+
+                {/* Wizualizacja karty decku */}
+                <div className="w-40 h-56 bg-white border-2 border-black flex flex-col items-center justify-center shadow-[6px_6px_0_0_rgba(0,0,0,1)] transform -rotate-2">
+                    <div className="text-5xl">🎴</div>
+                    <span className="mt-4 font-black italic text-black">
+                        CLASSIC
+                    </span>
+                </div>
+
+                <TactileButton variant="small" color="blue" className="px-2">
+                    <ChevronRight size={24} />
+                </TactileButton>
+            </div>
+
+            <TactileButton variant="small" color="yellow" className="w-3/4">
+                <Import size={16} className="mr-2" /> Importuj Deck
+            </TactileButton>
+        </div>
+    );
+};
