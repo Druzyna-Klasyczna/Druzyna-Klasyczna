8e12bb2465cd4cc8ba2882360919bd568ce68706
majkelrice <michalryz2003@gmail.com>
2026-05-10 12:47:59 +0200

    fix: added hardcoded questions

--- backend/game/deck.py
+++ backend/game/deck.py
@@ -1,10 +1,33 @@
 import random
 from game_exceptions import DrawPileEmptyException
+from card import QuestionCard, PowerUpCard, EffectCard, DebuffCard
+from card import PowerUpType, EffectType, DebuffType
 
 class Deck:
     def __init__(self):
-        self.cards = [1,2,3,4]*10 # ! hardcoded for now
+        self.cards = self._generate_mock_deck()
     
+    def _generate_mock_deck(self):
+        cards = []
+        
+        questions = [
+            QuestionCard("Jaki jest wynik działania 2 + 2 * 2?", ["8", "6", "4", "22"], 1),
+            QuestionCard("Który język używa asyncio?", ["C++", "Java", "Python", "HTML"], 2),
+            QuestionCard("Stolica Polski to:", ["Kraków", "Warszawa", "Poznań", "Gdańsk"], 1),
+            QuestionCard("Jak nazywa się metoda wywoływana przy tworzeniu klasy w Pythonie?", ["__start__", "__init__", "__main__", "__create__"], 1)
+        ]
+        cards.extend(questions)
+        
+        for _ in range(4):
+            cards.append(PowerUpCard(PowerUpType.SKIP))
+            cards.append(PowerUpCard(PowerUpType.MIRROR))
+            
+        for _ in range(2):
+            cards.append(EffectCard(EffectType.SHUFFLE_PLAYERS))
+            cards.append(DebuffCard(DebuffType.DRAW_PENALTY))
+            
+        return cards * 3
+
     def push_card(self, card):
         self.cards.append(card)
 
