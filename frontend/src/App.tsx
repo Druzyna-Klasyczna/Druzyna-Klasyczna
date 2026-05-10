6b86671f66019278837fc0d893ec980105c541e8
majkelrice <michalryz2003@gmail.com>
2026-05-10 09:08:23 +0200

    feat: added sync in lobby

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import HomePageWithSync from "./pages/HomePageWithSync";
 import { Lobby } from "./pages/Lobby";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePageWithSync />} />
         <Route path="/lobby" element={<Lobby></Lobby>} />
       </Routes>
     </BrowserRouter>
